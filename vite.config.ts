7b662a4472c5f48983016f548e4c3f5006294236
Abdullahawan00119 <abdullahawan00119@gmail.com>
2026-04-22 23:35:01 +0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+process.env.SERVER_PRESET = 'vercel';
+process.env.NITRO_PRESET = 'vercel';
+
 export default defineConfig({
   cloudflare: false,
   tanstackStart: {
